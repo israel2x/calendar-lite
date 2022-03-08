754013bdffb1ed5d434674d1f4222dc8e63435a4
israel2x <ezurita@fiec.espol.edu.ec>
2022-03-08 12:13:24 -0500

    Instalacion de librerias

---
+++ src/CalendarApp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AppRouter } from './routers/AppRouter'
+
+export const CalendarApp = () => {
+    return (
+        <div>
+            <AppRouter></AppRouter>
+            <h1>Calendar App</h1>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CalendarApp } from './CalendarApp';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CalendarApp></CalendarApp>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
